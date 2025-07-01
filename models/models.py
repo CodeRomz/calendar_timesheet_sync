899222f30677cae3f8c87badd48f64ee9516ce6d
root <root@localhost.localdomain>
2025-07-01 08:33:37 +0400

    Initial Commit

---
+++ models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class calendar_timesheet_sync(models.Model):
+#     _name = 'calendar_timesheet_sync.calendar_timesheet_sync'
+#     _description = 'calendar_timesheet_sync.calendar_timesheet_sync'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
